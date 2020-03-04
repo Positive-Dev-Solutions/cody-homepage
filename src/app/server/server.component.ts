9f4c263e4b7ba1e6d4d4d8492e5affc5507edab8
Cody Miller <cmiller221@gmail.com>
2020-03-03 22:14:11 -0600

    ngStyle Attribute Directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   thingy = "wow";
   numberThingy = 123;
+
+  constructor() {
+    this.thingy = Math.random() > 0.5 ? "No way" : "Yes way";
+  }
+
+  getColor() {
+    return this.thingy === "Yes way" ? 'green' : 'red';
+  }
 }
