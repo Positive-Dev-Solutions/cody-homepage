fedcdf966f2a311d0e424db4988138a16af66e39
Cody Miller <cmiller221@gmail.com>
2020-03-05 21:47:36 -0600

    Npm Packages Added and Beginning Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,9 +19,10 @@ import { MaterialModule } from './material.module';
   imports: [
     FormsModule,
     BrowserModule,
+    MaterialModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
 imports: [
   MatIconModule,
+  MatCardModule,
+  MatInputModule,
   MatButtonModule,
+  MatToolbarModule,
   MatFormFieldModule,
-  MatInputModule
 ],
 exports: [
   MatIconModule,
+  MatCardModule,
+  MatInputModule,
   MatButtonModule,
+  MatToolbarModule,
   MatFormFieldModule,
-  MatInputModule
 ],
 
 })
